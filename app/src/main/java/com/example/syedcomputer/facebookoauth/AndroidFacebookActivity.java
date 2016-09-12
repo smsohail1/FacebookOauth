df3ff0be4f1b82e189fb1bd7968c52f6c65c1552
SYED COMPUTER <vickkyr9@gmail.com>
2016-09-12 03:44:01 -0700

    version 1.0

---
+++ app/src/main/java/com/example/syedcomputer/facebookoauth/AndroidFacebookActivity.java
@@ -0,0 +1,21 @@
+package com.example.syedcomputer.facebookoauth;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.facebook.FacebookSdk;
+
+
+public class AndroidFacebookActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+    }
+}
